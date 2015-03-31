361cd9334bf2c118916864f2bd60367f6f09d49e
Sebastian Lux <sebastian.lux@live.com>
2015-03-31 07:28:50 +0200

    Add missing ViewModel

---
+++ Source/AssemblyInfo/ViewModels/AssemblyInfoViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using AssemblyInfo.Extensions;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace AssemblyInfo.ViewModels
+{
+    public class AssemblyInfoViewModel : ViewModelBase
+    {
+        private Assembly assembly = null;
+
+        public Task ReadAssemblyAsync(string location)
+        {
+            return Task.Run(() =>
+                            {
+                                this.assembly = Assembly.LoadFrom(location);
+                                RaisePropertyChanged(String.Empty);
+                            });
+        }
+
+        public String ProductVersion
+        {
+            get { return this.assembly.GetVersion(); }
+        }
+
+        public String FileVersion
+        {
+            get { return this.assembly.GetFileVersion(); }
+        }
+
+        public String InformationalVersion
+        {
+            get { return this.assembly.GetInformationalVersion(); }
+        }
+    }
+}
