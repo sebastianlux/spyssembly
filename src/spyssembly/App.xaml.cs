0a67097e177c808f8f8ff2156653e498a8070150
Sebastian Lux <sebastian.lux@live.com>
2016-12-21 15:38:41 +0100

    Cleanup App.xaml.cs code

--- src/spyssembly/App.xaml.cs
+++ src/spyssembly/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition.Hosting;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.Composition.Hosting;
 using System.Windows;
 
 namespace AssemblyInfo
