d66260f73e654a7676f06eec3adb1d23527b98df
Sebastian Lux <sebastian.lux@live.com>
2016-12-21 14:01:04 +0100

    Rename folders, solution and project; Remove obsolete files

--- src/spyssembly/Properties/AssemblyInfo.cs
+++ src/spyssembly/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AssemblyInfo")]
+[assembly: AssemblyTitle("spyssembly")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AssemblyInfo")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("spyssembly")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
