1191f71964032062f39aba3b24ae2c77dad9390d
Sebastian Lux <sebastian.lux@live.com>
2016-12-21 15:39:44 +0100

    Add a new wpf converter: NullValueConverter

---
+++ src/spyssembly/Converter/NullValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace spyssembly.Converter
+{
+    class NullValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value == null)
+            {
+                return NullValue;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        public Object NullValue { get; set; }
+    }
+}
